39f06752496af9c913b17c1541017851cda274c9
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-03 10:37:28 -0300

    feat(entity): add user entity

---
+++ src/user/domain/entities/user.entity.ts
@@ -0,0 +1,11 @@
+export type UserProps = {
+  name: string;
+  email: string;
+  password: string;
+  createAt?: Date;
+};
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createAt = this.props.createAt ?? new Date();
+  }
+}
