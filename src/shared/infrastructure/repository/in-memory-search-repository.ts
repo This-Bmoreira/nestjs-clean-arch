3602fc1a006ed4aa37b038fc5013d966d2cf3723
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-13 09:22:18 -0300

    feat: add InMemorySearchRepository

---
+++ src/shared/infrastructure/repository/in-memory-search-repository.ts
@@ -0,0 +1,12 @@
+import { Entity } from '../../domain/entities/entity';
+import { InMemoryRepository } from './in-memory-repository';
+import { SearchRepositoryInterface } from './search-repository-contracts';
+
+export abstract class InMemorySearchRepository<E extends Entity<any>>
+  extends InMemoryRepository<E>
+  implements SearchRepositoryInterface<E, any, any>
+{
+  search(props: any): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
