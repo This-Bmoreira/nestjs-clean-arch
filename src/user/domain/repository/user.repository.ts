6f83379f1bd9104d0444523e552c82481c5e8dee
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-16 07:29:35 -0300

    refactor: updated UserRepository interface
    Updated the UserRepository interface to include a namespace named UserRepository. Introduced a
    Filter type within the UserRepository namespace to specify the type for filtering. Created custom
    SearchParams and SearchResult classes within the UserRepository namespace to provide more specific
    typing for search-related operations. Defined the SearchParams class as an extension of the default
    SearchParams class with the added Filter type. Defined the SearchResult class as an extension of the
    default SearchResult class with the added Filter type. Modified the Repository interface to utilize
    the custom SearchParams and SearchResult classes in the SearchRepositoryInterface. Added the
    emailExists method to the Repository interface for checking if an email address already exists.
    Improved the code organization and readability by grouping related types and classes within the
    UserRepository namespace.

--- src/user/domain/repository/user.repository.ts
+++ src/user/domain/repository/user.repository.ts
@@ -1,8 +1,24 @@
-import { SearchRepositoryInterface } from '../../../shared/infrastructure/repository/search-repository-contracts';
-import { UserEntity } from '../entities/user.entity';
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchRepositoryInterface,
+} from '../../../shared/infrastructure/repository/search-repository-contracts';
 
-export interface UserRepository
-  extends SearchRepositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>;
-  checkEmailExists(email: string): Promise<void>;
+import { UserEntity } from '../entities/user.entity';
+export namespace UserRepository {
+  export type Filter = string;
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> {}
+
+  export interface Repository
+    extends SearchRepositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>;
+    emailExists(email: string): Promise<void>;
+  }
 }
