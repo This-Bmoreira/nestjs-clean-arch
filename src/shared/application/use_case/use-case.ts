55aa5b142a59f919e36fdd070c89491ea69d1dbd
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-18 10:08:54 -0300

    feat: added 'findAll' method to the ApplicationAction interface

--- src/shared/application/use_case/use-case.ts
+++ src/shared/application/use_case/use-case.ts
@@ -1,4 +1,5 @@
 export interface ApplicationAction<Input, Output> {
   executeSignup?(input: Input): Output | Promise<Output>;
   findOne?(input: Input): Output | Promise<Output>;
+  findAll?(input: Input): Promise<Output>;
 }
