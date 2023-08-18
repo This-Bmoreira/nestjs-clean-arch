ee3e9012125005e379896220586b12513c4b481d
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-18 10:18:20 -0300

    refactor: refactor OutputUser and UserOutputMapper for consistency
    Updated the OutputUser type definition to use a more consistent format. Refactored the
    UserOutputMapper class to use the toJSON method of the UserEntity for consistency and improved code
    readability.

--- src/user/application/dto/output-user.dto.ts
+++ src/user/application/dto/output-user.dto.ts
@@ -1,7 +1,15 @@
-export class OutputUser {
+import { UserEntity } from '../../domain/entities/user.entity';
+
+export type OutputUser = {
   id: string;
   name: string;
   email: string;
   password: string;
   createdAt: Date;
+};
+
+export class UserOutputMapper {
+  static toOutput(entity: UserEntity): OutputUser {
+    return entity.toJSON();
+  }
 }
