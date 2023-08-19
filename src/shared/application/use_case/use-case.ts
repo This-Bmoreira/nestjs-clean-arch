87b6ccc582628ebd2cd8662bc8b51cbef346527f
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-19 07:20:16 -0300

    feat: add 'update' method to the ApplicationAction interface

--- src/shared/application/use_case/use-case.ts
+++ src/shared/application/use_case/use-case.ts
@@ -2,4 +2,5 @@ export interface ApplicationAction<Input, Output> {
   executeSignup?(input: Input): Output | Promise<Output>;
   findOne?(input: Input): Output | Promise<Output>;
   findAll?(input: Input): Promise<Output>;
+  update?(input: Input): Promise<Output>;
 }
