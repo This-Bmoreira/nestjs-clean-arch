271057d691ca638b1daec5b5d5d1081e1c4a1b67
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-08-21 09:29:01 -0300

    feat: configure user module and dependency injection

---
+++ src/user/infrastructure/user.module.ts
@@ -0,0 +1,85 @@
+import { Module } from '@nestjs/common';
+import { DetailUserService } from '../application/use_case/detail-user.service';
+import { DetailUsersService } from '../application/use_case/detail-users.service';
+import { PasswordModificationService } from '../application/use_case/modify-password.service';
+import { ModifyUserService } from '../application/use_case/modify-user.service';
+import { RemoveUserService } from '../application/use_case/remove-user.service';
+import { SignInService } from '../application/use_case/signin.service';
+import { SignupService } from '../application/use_case/signup.service';
+import { UserRepository } from '../domain/repository/user.repository';
+import { BcryptPasswordHasher } from '../provider/hash-provider/bcryptjs-hash-provider';
+import { UserInMemoryRepository } from './database/in-memory/repository/user-in-memory.repository';
+import { UserController } from './user.controller';
+
+@Module({
+  controllers: [UserController],
+  providers: [
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'hashProvider',
+      useClass: BcryptPasswordHasher,
+    },
+    {
+      provide: SignupService.Create,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: BcryptPasswordHasher,
+      ) => {
+        return new SignupService.Create(userRepository, hashProvider);
+      },
+      inject: ['UserRepository', 'hashProvider'],
+    },
+    {
+      provide: SignInService.SignIn,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: BcryptPasswordHasher,
+      ) => {
+        return new SignInService.SignIn(userRepository, hashProvider);
+      },
+      inject: ['UserRepository', 'hashProvider'],
+    },
+    {
+      provide: RemoveUserService.UserProfile,
+      useFactory: (userRepository: UserRepository.Repository) => {
+        return new RemoveUserService.UserProfile(userRepository);
+      },
+      inject: ['UserRepository'],
+    },
+    {
+      provide: ModifyUserService.UserProfileModification,
+      useFactory: (userRepository: UserRepository.Repository) => {
+        return new ModifyUserService.UserProfileModification(userRepository);
+      },
+      inject: ['UserRepository'],
+    },
+    {
+      provide: PasswordModificationService.passwordModification,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: BcryptPasswordHasher,
+      ) => {
+        return new SignInService.SignIn(userRepository, hashProvider);
+      },
+      inject: ['UserRepository', 'hashProvider'],
+    },
+    {
+      provide: DetailUsersService.UsersProfile,
+      useFactory: (userRepository: UserRepository.Repository) => {
+        return new DetailUsersService.UsersProfile(userRepository);
+      },
+      inject: ['UserRepository'],
+    },
+    {
+      provide: DetailUserService.UserProfile,
+      useFactory: (userRepository: UserRepository.Repository) => {
+        return new DetailUserService.UserProfile(userRepository);
+      },
+      inject: ['UserRepository'],
+    },
+  ],
+})
+export class UserModule {}
