42159e6cade6efcbe97d5493987be8e2ca019300
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-09-06 09:22:10 -0300

    feat: added Swagger API documentation to presenter

--- src/user/infrastructure/presentation/user.presenter.ts
+++ src/user/infrastructure/presentation/user.presenter.ts
@@ -1,11 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { CollectionPresenter } from '../../../shared/infrastructure/presentation/collection.presenter';
 import { OutputUser } from '../../application/dto/output-user.dto';
 import { DetailUsersService } from '../../application/use_case/detail-users.service';
+
 export class UserPresenter {
+  @ApiProperty({ description: 'Identificação do usuário' })
   id: string;
+
+  @ApiProperty({ description: 'Nome do usuário' })
   name: string;
+
+  @ApiProperty({ description: 'E-mail do usuário' })
   email: string;
+
+  @ApiProperty({ description: 'Data de criação do usuário' })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date;
 
